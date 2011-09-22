a9eb72745359a623372ce1027c92f9c9f8d6402b
Justin Grimes <justin.mgrimes@gmail.com>
2011-09-22 11:28:08 -0400

    updated for GSL

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,4 @@
+require 'gslng'
 class Calculator
   def push(n)
     @args ||= []
@@ -8,5 +9,12 @@ class Calculator
     @args.inject(0){|n,sum| sum+=n}
   end
 
+  def mean
+    v = GSLng::Vector.new(2,false)
+    v[0] = @args[0]
+    v[1] = @args[1]
+    v.mean
+
+  end
 
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -5,10 +5,21 @@ describe Calculator do
   describe ".add" do
     it "returns 1 when 0 and 1 are summed" do
       calculator = Calculator.new
-     calculator.push 0
+      calculator.push 0
       calculator.push 1
       actual_result = calculator.add
       actual_result.should be(1)
+    end
+  end
+
+  describe ".mean" do
+    it "calculates the average of a list of numbers" do
+      calculator = Calculator.new
+      calculator.push 2
+      calculator.push 4
+      actual_result = calculator.mean
+      actual_result.should == 3.0
+
     end
   end
 end
